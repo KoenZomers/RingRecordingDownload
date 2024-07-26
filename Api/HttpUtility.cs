e98b9581e2ebff2b29ec8b5d69f2b9fdea7143dc
Daniel Espinosa <luespino@microsoft.com>
2024-07-26 14:34:15 -0700

    Adds verbosity if call to ring doesn't succeed

--- Api/HttpUtility.cs
+++ Api/HttpUtility.cs
@@ -90,7 +90,7 @@ namespace KoenZomers.Ring.Api
             // Send the request to the webserver
             var response = await _httpClient.SendAsync(request);
 
-            switch(response.StatusCode)
+            switch (response.StatusCode)
             {
                 case HttpStatusCode.TooManyRequests:
                     throw new Exceptions.ThrottledException();
@@ -101,6 +101,13 @@ namespace KoenZomers.Ring.Api
 
             // Return the response from the server
             var responseFromServer = await response.Content.ReadAsStringAsync();
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Response to get contents was not successful.");
+                Console.WriteLine("Response Status: {0}", response.StatusCode);
+                Console.WriteLine("Response: {0}", responseFromServer);
+            }
+
             return responseFromServer;
         }
 
